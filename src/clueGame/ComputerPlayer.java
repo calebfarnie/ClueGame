295c5be3dbc8cb5c4ed73423cd17f948eb8a48cc
Caleb Farnie <cfarnie@mymail.mines.edu>
2020-11-06 12:21:48 -0700

    passed tests for create suggestion

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -19,7 +19,7 @@ public class ComputerPlayer extends Player {
 
 	@Override
 	public void updateHand(Card card) {
-		super.hand.add(card);		
+		super.hand.add(card);
 	}
 	
 	@Override
@@ -28,9 +28,37 @@ public class ComputerPlayer extends Player {
 	}
 	
 	@Override
-	public Solution createSuggestion() {
-		// TODO
-		return new Solution();
+	public Solution createSuggestion(String room, ArrayList<Card> deck) {
+		Solution mySoln = new Solution();
+		ArrayList<Card> potentialWeapon = new ArrayList<Card>();
+		ArrayList<Card> potentialPerson = new ArrayList<Card>();
+		
+		// set room
+		mySoln.room = new Card(room, CardType.ROOM);
+		
+		// set weapon
+		for(Card card : deck) {
+			if(!seenCards.contains(card) && card.getType() == CardType.WEAPON) {
+				potentialWeapon.add(card);
+			}
+		}
+		
+		// return random (still returns the only card if only one)
+		Collections.shuffle(potentialWeapon);
+		mySoln.weapon = potentialWeapon.get(0);
+		
+		// set person
+		for(Card card : deck) {
+			if(!seenCards.contains(card) && card.getType() == CardType.PERSON) {
+				potentialPerson.add(card);
+			}
+		}
+		
+		// return random (still returns the only card if only one)
+		Collections.shuffle(potentialPerson);
+		mySoln.person = potentialPerson.get(0);
+		
+		return mySoln;
 	}
 	
 	@Override
