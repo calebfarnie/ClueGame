69776fe4a44150905800626f4ef8fbea780de66c
Caleb Farnie <cfarnie@mymail.mines.edu>
2020-11-10 17:45:45 -0700

    added ClueGame JFrame class and added the panels

---
+++ src/clueGame/ClueGame.java
@@ -0,0 +1,33 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class ClueGame extends JFrame {
+
+	public ClueGame(Player person, Board board) {
+		setTitle("Clue");
+		setSize(700, 700);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		add(new GameCardPanel(person, board), BorderLayout.EAST);
+		
+		add(new GameControlPanel(), BorderLayout.SOUTH);
+	}
+
+	public static void main(String[] args) {
+		// get the instance of the game board
+		Board board = Board.getInstance();
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		board.initialize();
+
+		// get the human player, Luke Skywalker
+		Player person = board.getPlayer("Luke Skywalker");
+		ClueGame gui = new ClueGame(person, board);	
+		gui.setVisible(true);
+	}
+
+}
