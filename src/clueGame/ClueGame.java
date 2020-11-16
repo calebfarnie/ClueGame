9a720380ef90700a5489cbadd9c88c337cb7c13e
Caleb Farnie <cfarnie@mymail.mines.edu>
2020-11-15 19:46:02 -0700

    added splash screen

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -3,10 +3,14 @@ package clueGame;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class ClueGame extends JFrame {
 
@@ -32,6 +36,19 @@ public class ClueGame extends JFrame {
 		Player person = board.getPlayer("Luke Skywalker");
 		ClueGame gui = new ClueGame(person, board);	
 		gui.setVisible(true);
+		
+		// display splash screen
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(0,1));
+		JLabel line1 = new JLabel("You are " + person.getName() + ".");
+		JLabel line2 = new JLabel("Can you find the solution");
+		JLabel line3 = new JLabel("before the Computer players?");
+		line1.setHorizontalAlignment(SwingConstants.CENTER);
+		line2.setHorizontalAlignment(SwingConstants.CENTER);
+		line3.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(line1);
+		panel.add(line2);
+		panel.add(line3);
+		JOptionPane.showMessageDialog(null, panel, "Welcome to Clue!", JOptionPane.INFORMATION_MESSAGE);
 	}
-
 }
